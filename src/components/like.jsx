011e067d73292fa7a4e547f5251f196ff187f234
mightyhyuk <118177277+mightyhyuk@users.noreply.github.com>
2022-11-19 22:51:58 +0900

    Create Like component

---
+++ src/components/like.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Like extends Component {
+  state = {};
+  render() {
+    return <i className="fa-solid fa-heart"></i>;
+  }
+}
+
+export default Like;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import Like from "./like";
 
 class Movies extends Component {
   state = {
@@ -25,6 +26,7 @@ class Movies extends Component {
               <th>Stock</th>
               <th>Rate</th>
               <th></th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
@@ -35,6 +37,9 @@ class Movies extends Component {
                   <td>{movie.genre.name}</td>
                   <td>{movie.numberInStock}</td>
                   <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <Like />
+                  </td>
                   <td>
                     <button
                       onClick={() => this.handleDelete(movie)}
