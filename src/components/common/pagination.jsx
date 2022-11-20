7ca42bb83aba35413d34aa286c7cecd633b1a2bb
mightyhyuk <118177277+mightyhyuk@users.noreply.github.com>
2022-11-20 11:32:38 +0900

    Display pages with Pagination

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,22 +1,18 @@
 const Pagination = (props) => {
+  const { itemCount, pageSize } = props;
+  const pageCount = Math.ceil(itemCount / pageSize);
+  if (pageCount === 1) return null;
+  const pages = [];
+  for (let i = 1; i <= pageCount; i++) pages.push(i);
+
   return (
     <nav>
       <ul className="pagination">
-        <li className="page-item">
-          <a className="page-link" href="#">
-            1
-          </a>
-        </li>
-        <li className="page-item">
-          <a className="page-link" href="#">
-            2
-          </a>
-        </li>
-        <li className="page-item">
-          <a className="page-link" href="#">
-            3
-          </a>
-        </li>
+        {pages.map((page) => (
+          <li key={page} className="page-item">
+            <a className="page-link">{page}</a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
